2a621be19fa123e41abdcbddd99a987800985ad4
たけのこII <yu2.aoki0229.@gmail.com>
2026-02-21 18:59:40 +0900

    あい

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,15 @@
 // src/index.ts
 console.log("Hello, World!");
+addEventListener("DOMContentLoaded", () => {
+  const headerButton = document.getElementById("header-button");
+  headerButton.addEventListener("click", () => {
+    const dropdown = document.getElementById("dropdown");
+    if (dropdown.style.maxHeight.length === 0) {
+      dropdown.style.maxHeight = dropdown.scrollHeight + "px";
+      headerButton.textContent = "×";
+    } else {
+      dropdown.style.maxHeight = "";
+      headerButton.textContent = "≡";
+    }
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
 console.log("Hello, World!");
+
+addEventListener("DOMContentLoaded", () => {
+    const headerButton = document.getElementById("header-button")!;
+
+    headerButton.addEventListener("click", () => {
+        const dropdown = document.getElementById("dropdown")!;
+
+        if (dropdown.style.maxHeight.length === 0) {
+            // 閉じてるとき -> 開く
+            dropdown.style.maxHeight = dropdown.scrollHeight + "px";
+            headerButton.textContent = "×"
+        }
+        else {
+            // 開いてるとき -> 閉じる
+            dropdown.style.maxHeight = "";
+            headerButton.textContent = "≡"
+        }
+    });
+});
